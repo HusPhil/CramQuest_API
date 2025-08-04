4cced15f1bba25e7d509754cbec396188772d638
Fhil Joshua <husphil200@gmail.com>
2025-08-04 21:04:21 +0800

    added a new enum for the uses available for an item and a new schema for response an item use and a request schema for equipping skin

--- app/schemas/player_inventory_item_schema.py
+++ app/schemas/player_inventory_item_schema.py
@@ -1,5 +1,13 @@
+from enum import Enum
 from pydantic import BaseModel
 from typing import Optional
+from app.schemas.reward_schema import RewardItemRead
+
+
+class ItemUses(str, Enum):
+    EQUIP = "equipped"
+    UNEQUIP = "unequipped"
+    USE = "used"
 
 
 class PlayerInventoryItemRead(BaseModel):
@@ -8,3 +16,12 @@ class PlayerInventoryItemRead(BaseModel):
     reward_id: int
     quantity: int
     equipped_slot: Optional[str]
+    item: RewardItemRead
+
+
+class EquipSkinRequest(BaseModel):
+    skin_url: str
+
+
+class UseItemResponse(BaseModel):
+    status: ItemUses
