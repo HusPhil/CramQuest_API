fb081d5a625ef241c001fe6d793450df2a188804
Fhil Joshua <husphil200@gmail.com>
2025-08-12 11:36:48 +0800

    added domain in set cookies

--- app/core/database.py
+++ app/core/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import NullPool
 from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -19,10 +20,7 @@ engine = create_async_engine(
     settings.DATABASE_URL,
     # echo=True,
     future=True,
-    pool_size=5,
-    max_overflow=3,
-    pool_timeout=60,
-    pool_recycle=900,
+    poolClass=NullPool,
 )
 
 AsyncSessionLocal = sessionmaker(
